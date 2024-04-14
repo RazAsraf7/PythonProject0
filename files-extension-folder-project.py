3750f5197b1e2243ca5eea457a34d1ecc6dabe60
Raz Asraf <razasraf7@gmail.com>
2024-04-14 18:16:53 +0300

    Fixed and improved code

--- files-extension-folder-project.py
+++ files-extension-folder-project.py
@@ -1,19 +1,28 @@
 import os
 import inspect
 default_directory = "C:\\Users\\USER\\Desktop\\DevOps\\Sela Project\\pythonProject1\\filextesnionsproj"
-used_directory = "C:\\Users\\USER\\Desktop\\DevOps\\Sela Project\\pythonProject1\\filextesnionsproj\\filextesnionsproj_copy"
 
 
+def given_folder(which_folder=default_directory):
+    folderlist.append(which_folder)
 
-def all_files_lister(source_folder=default_directory):
-    ALL_FILES = os.listdir(source_folder)
+#Don't forget to put the directory path at used directory variable!
+folderlist = ['a']
+
+given_folder()
+
+used_directory = folderlist[-1]
+print(used_directory)
+
+def all_files_lister():
+    ALL_FILES = os.listdir(used_directory)
     return ALL_FILES
 
 
-def non_duplicates_extensions_list(source_folder=default_directory):
+def non_duplicates_extensions_list():
     file_extensions = []
     file_extensions_new = []
-    for file in all_files_lister(source_folder):
+    for file in all_files_lister(used_directory):
         helplist = file.split(".")
         file_extensions.append(helplist[-1])
     for extension in file_extensions:
@@ -22,8 +31,8 @@ def non_duplicates_extensions_list(source_folder=default_directory):
     return file_extensions_new
 
 
-def folder_creator(source_folder=default_directory):
-    for file_extension in non_duplicates_extensions_list(source_folder):
+def folder_creator():
+    for file_extension in non_duplicates_extensions_list(used_directory):
         if file_extension in all_files_lister(default_directory):
             print(f"Folder {file_extension} already exists")
             continue
@@ -33,19 +42,19 @@ def folder_creator(source_folder=default_directory):
     return
 
 
-def file_mover(source_folder=default_directory ,to_folder=default_directory):
-    for file_name in all_files_lister(source_folder):
+def file_mover():
+    for file_name in all_files_lister(used_directory):
         if "." not in file_name: 
             continue
         else:
             splittup = file_name.split(".")
             fextension = splittup[-1]
-        os.replace(source_folder + f"\\{file_name}", to_folder + f"\\{fextension}\\{file_name}")
+        os.replace(used_directory + f"\\{file_name}", default_directory + f"\\{fextension}\\{file_name}")
         print(f"Moved {file_name} to {fextension}")
     return
 
-
-all_files_lister(used_directory)
-non_duplicates_extensions_list(used_directory)
-folder_creator(used_directory)
-file_mover(used_directory)
+used_directory = "C:\\Users\\USER\\Desktop\\DevOps\\Sela Project\\pythonProject1\\filextesnionsproj\\filextesnionsproj_copy"
+all_files_lister()
+non_duplicates_extensions_list()
+folder_creator()
+file_mover()
---
+++ test.py
@@ -0,0 +1,6 @@
+somelist = ['a']
+def testfunc(something_to_add):
+    somelist.append(something_to_add)
+
+testfunc("hatchapuri")
+print(somelist)
